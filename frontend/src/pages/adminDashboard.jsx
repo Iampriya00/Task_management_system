611a80e6d5850286693001eedb9a1c9993db3047
Priya Chakraborty <140253223+Iampriya00@users.noreply.github.com>
2024-12-20 17:20:07 +0530

    Commit: admin_profile

--- frontend/src/pages/adminDashboard.jsx
+++ frontend/src/pages/adminDashboard.jsx
@@ -1,13 +1,41 @@
-import { Button } from "@/components/ui/button";
-import { handleLogout } from "@/services/authservice";
-import React from "react";
+import React, { useEffect } from "react";
+import SideBar from "@/components/Dashboard/sideBar";
+import { useAppSelector } from "@/store/hooks";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 function AdminDashboard() {
+  const user = useAppSelector((state) => state.user.user);
+
   return (
-    <div>
-      <Button variant="destructive" onClick={() => handleLogout()}>
-        Log Out
-      </Button>
+    <div className="flex">
+      <SideBar />
+      <div className="w-3/4 flex h-screen">
+        <div className="container p-20 ">
+          <h1 className="text-2xl mb-6 text-center font-semibold">
+            Use Your Information
+          </h1>
+          <div className="flex justify-center mb-3">
+            <Avatar>
+              <AvatarImage src={user.profileImg} />
+              <AvatarFallback>{user.username[0].toUpperCase()}</AvatarFallback>
+            </Avatar>
+          </div>
+          <div className="text-center">
+            <p className="text-lg font-medium text-gray-700">
+              ID: <span className="text-gray-500">{user._id}</span>
+            </p>
+            <p className="text-lg font-medium text-gray-700">
+              Username: <span className="text-gray-500">{user.username}</span>
+            </p>
+            <p className="text-lg font-medium text-gray-700">
+              Email: <span className="text-gray-500">{user.email}</span>
+            </p>
+            <p className="text-lg font-medium text-gray-700">
+              Phone: <span className="text-gray-500">{user.phone}</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
