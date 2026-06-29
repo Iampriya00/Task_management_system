edddff63f16a59116f7dfbe5c3f1e2420f8051b7
Priya Chakraborty <priyachakrab01411@gmail.com>
2026-06-29 15:15:59 +0530

    commit: Proper Database connect 1

--- Backend/connection/connection.js
+++ Backend/connection/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
