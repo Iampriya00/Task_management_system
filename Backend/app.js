db8c1cc13009d59e7cacd1fc85cd52cd1b4ee03a
Priya Chakraborty <priyachakrab01411@gmail.com>
2026-06-29 14:59:35 +0530

    Revert "database"
    This reverts commit 23cb60a9ca4c018915ecbf679d1ee980776843ff.

--- Backend/app.js
+++ Backend/app.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-
-const connectToDatabase = require("./connection/connection");
-connectToDatabase();
+require("./connection/connection");
 
 // Import routes
 const user = require("./Routes/user");
@@ -18,32 +16,30 @@ const department = require("./Routes/department");
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/api/users", user);
-app.use("/api/tasks", task);
-app.use("/api/clock", clock);
-app.use("/api/leaves", leave);
-app.use("/api/projects", project);
-app.use("/api/departments", department);
+// Use routes
+app.use(user);
+app.use(task);
+app.use(clock);
+app.use(leave);
+app.use(project);
+app.use(department);
+
+// Error Handling Middleware:
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
 
 // Root route
 app.get("/", (req, res) => {
   res.send("Hello baby");
 });
 
-// 404 handler
-app.use((req, res) => {
-  res.status(404).json({ message: "Route not found" });
-});
-
-// Error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something went wrong!");
-});
-
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on: ${PORT}`);
-});
+app
+  .listen(PORT, () => {
+    console.log(`🚀 Server is running on: ${PORT}`);
+  })
+  .on("error", (err) => {
+    console.error("Error starting the server", err);
+  });
